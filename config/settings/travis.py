f95d02fff493773461c97c67e2c121ae270572c9
Alexandre Prieto Pantoja <alexandreprietopantoja@MacBook-Pro.local>
2020-11-09 10:54:29 +0100

    add travis config + tests folders

---
+++ config/settings/travis.py
@@ -0,0 +1,15 @@
+from . import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+        'TEST': {
+            'NAME': 'mytestdatabase',
+        }
+    },
+}
