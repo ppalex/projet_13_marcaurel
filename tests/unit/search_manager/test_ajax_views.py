b3846c08c10aa359bc6a324d665c934b6064bae4
Alexandre Prieto Pantoja <alexandreprietopantoja@MacBook-Pro.local>
2021-01-12 21:29:51 +0100

    add ajax view test

--- tests/unit/search_manager/test_ajax_views.py
+++ tests/unit/search_manager/test_ajax_views.py
@@ -1,6 +1,38 @@
 from django.test import RequestFactory, TestCase
-from search_manager.ajax_view import (autocomplete_city,
+from search_manager.ajax_view import (filter_match_view, autocomplete_city,
                                       autocomplete_player)
+from users.models.user import User
+import json
+import pdb
+
+
+class FilterMatchView(TestCase):
+    fixtures = ["data.json"]
+
+    @classmethod
+    def setUpTestData(self):
+        self.factory = RequestFactory()
+
+        self.user = User.objects.create_user(
+            username='user3', email='user3@gmail.com', password='password')
+
+    def test_classification_filter(self):
+        self.client.login(username='user3', password='password')
+        data = {'classification': 'public',
+                'city': '',
+                'available_place': '',
+                'start_fixture': '',
+                'location': '',
+                }
+
+        request = self.factory.post('/search/match/filter', data)
+
+        request.user = self.user
+
+        response = filter_match_view(request)
+        json_response = json.loads(response.content)
+        match = json_response[0]
+        self.assertEqual(match['classification'], 'public')
 
 
 class AutocompleteCityTest(TestCase):
