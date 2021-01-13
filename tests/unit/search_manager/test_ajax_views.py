9cffda5f8dcff9e4bd183dd052919a057e30b62f
Alexandre Prieto Pantoja <alexandreprietopantoja@MacBook-Pro.local>
2021-01-13 08:50:35 +0100

    update classification filter test + add city filter test

--- tests/unit/search_manager/test_ajax_views.py
+++ tests/unit/search_manager/test_ajax_views.py
@@ -31,8 +31,35 @@ class FilterMatchView(TestCase):
 
         response = filter_match_view(request)
         json_response = json.loads(response.content)
-        match = json_response[0]
-        self.assertEqual(match['classification'], 'public')
+
+        self.assertSetEqual(
+            set(map(lambda x: x['classification'], json_response)), {'public'})
+
+    def test_city_filter(self):
+        self.client.login(username='user3', password='password')
+        data = {'classification': '',
+                'city': 'Anvers',
+                'available_place': '',
+                'start_fixture': '',
+                'location': '',
+                }
+
+        request = self.factory.post('/search/match/filter', data)
+        request.user = self.user
+        response = filter_match_view(request)
+        json_response = json.loads(response.content)
+        
+        self.assertSetEqual(
+            set(map(lambda x: x['id'], json_response)), {2})
+
+    def test_available_place_filter(self):
+        pass
+
+    def test_start_fixture_filter(self):
+        pass
+
+    def test_location_filter(self):
+        pass
 
 
 class AutocompleteCityTest(TestCase):
