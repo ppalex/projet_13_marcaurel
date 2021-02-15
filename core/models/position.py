bc953ba892d2c52d36a02e10f081a957dba6ba68
Alexandre Prieto Pantoja <alexandreprietopantoja@MacBook-Pro.local>
2021-02-15 08:05:44 +0100

    add position populate db + correct position name

--- core/models/position.py
+++ core/models/position.py
@@ -10,4 +10,4 @@ class Position(models.Model):
     position_name = models.CharField(max_length=255, choices=POSITION_CHOICES)
 
     def __str__(self):
-        return f"{self.position_name}"
+        return f"{self.get_position_name_display()}"
