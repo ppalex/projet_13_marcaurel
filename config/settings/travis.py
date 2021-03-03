05b804f5ddcd59fd4f698570d7bda3e6d9e3d200
Alexandre Prieto Pantoja <alexandreprietopantoja@MacBook-Pro.local>
2021-03-03 17:40:31 +0100

    new travis settings

--- config/settings/travis.py
+++ config/settings/travis.py
@@ -4,15 +4,15 @@ import os
 FIXTURE_DIRS = (os.path.join('tests', 'fixtures'),)
 
 DATABASES = {
-    "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE"),
-        "NAME": os.environ.get("SQL_DATABASE"),
-        "USER": os.environ.get("SQL_USER"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD"),
-        "HOST": os.environ.get("SQL_HOST"),
-        "PORT": os.environ.get("SQL_PORT"),
-        "TEST": {
-            'NAME': os.environ["SQL_DATABASE"]
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+        'TEST': {
+            'NAME': 'mytestdatabase',
         }
-    }
+    },
 }
